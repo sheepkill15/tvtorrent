5ea463a0abb52ca3bceae0b3d1f00e2fe04dfd83
sheepkill15 <simonarons15@gmail.com>
2021-01-13 19:54:13 +0200

    Use STD's hash instead of my shady implementation

--- src/hash.h
+++ src/hash.h
@@ -16,20 +16,21 @@ public:
 //        if(found != m_Cache.end()) {
 //            return found->second;
 //        }
-        size_t randomizer = 100;
-        for (char i : myString)
-        {
-            randomizer += i;
-        }
-        randomizer += myString.size() * myString[0];
-        randomizer += myString.back();
-        randomizer *= myString.back();
+//        size_t randomizer = 100;
+//        for (char i : myString)
+//        {
+//            randomizer += i;
+//        }
+//        randomizer += myString.size() * myString[0];
+//        randomizer += myString.back();
+//        randomizer *= myString.back();
 
         //m_Cache.insert(std::make_pair(myString, randomizer));
-        return randomizer;
+        return m_Hasher(myString);
     }
 private:
     /// Caching
+    inline static std::hash<std::string> m_Hasher;
     //inline static std::unordered_map<std::string, size_t> m_Cache;
 };
 
